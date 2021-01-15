3cfed2c60866b3b2fc319bc321fa6ab884551845
Nick Hafkemeyer <mail@nhafkemeyer.com>
2021-01-15 16:31:00 +0100

    extend tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,33 +5,34 @@ module.exports = {
   ],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          DEFAULT: 'var(--primary)',
+          dark: 'var(--primary-dark)'
+        },
+        secondary: 'var(--secondary)',
+        'light-gray': 'var(--light-gray)',
+        background: 'var(--background)',
+        'dark-accent': 'var(--dark-accent)',
+        'light-accent': 'var(--light-accent)',
+      },
+      padding: {
+        xs: '4px',
+        sm: '10px',
+        md: '20px',
+        lg: '40px',
+        xl: '80px'
+      },
+      maxWidth: {
+        '1/4': '25%',
+        '1/3': '30%',
+        '1/2': '50%',
+        '2/3': '60%',
+      },
+    },
     container: {
       center: true,
-    },
-    padding: {
-      xs: '4px',
-      sm: '10px',
-      md: '20px',
-      lg: '40px',
-      xl: '80px'
-    },
-    maxWidth: {
-      '1/4': '25%',
-      '1/3': '30%',
-      '1/2': '50%',
-      '2/3': '60%',
-    },
-    colors: {
-      primary: {
-        DEFAULT: 'var(--primary)',
-        dark: 'var(--primary-dark)'
-      },
-      secondary: 'var(--secondary)',
-      gray: 'var(--light-gray)',
-      background: 'var(--background)',
-      'dark-accent': 'var(--dark-accent)',
-      'light-accent': 'var(--light-accent)',
     },
   },
   variants: {
